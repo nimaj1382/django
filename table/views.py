bee5517c4c5e3e61db1706dce8e71ba58f83a0f8
/\/ / /\/\ / <njpc@nimaj1382@gmail.com>
2022-08-27 13:49:53 +0430

    models added

--- table/views.py
+++ table/views.py
@@ -1,7 +1,11 @@
+import os
+from dotenv import load_dotenv
 from django.shortcuts import render
 from django.http import HttpResponse as hr
 
-view_file = open('./view.html')
+load_dotenv()
+
+view_file = open(os.getenv('path_to_html'))
 res = view_file.read()
 
 def index(req):
